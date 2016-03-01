920c70d1646e87edd8452a1ed8aee2c831c2b9b3
Guilherme Parmezani <gparmezani@fahrenheitmarketing.com>
2016-03-01 15:42:36 -0600

    Fixed How It Works click behaviour

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -75,94 +75,28 @@
       $(window).scrollTo(0, 500);
     });
 
+    // How It Works Block
+    var howItWorksHeight = $('.how-it-works-block-wrapper').height() + 60;
+
     $(window).load(function(){
-      var howItWorksHeight = $('.how-it-works-block-wrapper').height() + 60;
       $('.how-it-works-block-wrapper').css('top', -howItWorksHeight);
-
-      $('a.collapse-toggle:not(.dismiss)').click(function(e){
-        e.preventDefault();
-        $($(this).attr('href')).toggleClass('animated');
-        $($(this).attr('href')).removeAttr('style');
-        
-        if ($($(this).attr('href')).hasClass('animated')) {
-          $('.main').css('margin-top', howItWorksHeight);
-        } else {
-          $('.main').removeAttr('style');
-          $($(this).attr('href')).css('top', -howItWorksHeight);
-        }
-      });
-
-      $('.dismiss').click(function(e){
-        e.preventDefault();
-        $($(this).attr('href')).removeClass('animated');
-        $($(this).attr('href')).css('top', -howItWorksHeight);
-        $('.main').removeAttr('style');
-      });
-
       $('.how-it-works-block-wrapper').removeClass('no-opacity');
     });
+    
+    $('a.collapse-toggle:not(.dismiss)').click(function(e){
+      e.preventDefault();
+      $($(this).attr('href')).addClass('animated');
+      $($(this).attr('href')).removeAttr('style');
+      $('.main').css('margin-top', howItWorksHeight);
+      $(window).scrollTo(0, 500);
+    });
 
-
-    // $('input:not(.datepicker), textarea, select').focusin(function() {
-    // 	placeHolder = $(this).attr('placeholder');
-    // 	$(this).attr('placeholder', '');
-    // });
-    // $('input:not(.datepicker), textarea, select').focusout(function() {
-    // 	$(this).attr('placeholder', placeHolder);
-    // });
-
-    // $('.delegate-now').click(function(e) {
-    // 	e.preventDefault();
-    // 	$(window).scrollTo('#delegate-form', 800);
-    // });
-
-    // $('footer .brand').click(function(e) {
-    // 	e.preventDefault();
-    // 	$(window).scrollTo('#top', 800);
-    // });
-
-    // $('.down-arrow a').click(function(e) {
-    // 	e.preventDefault();
-    // 	$(window).scrollTo('#second-section', 600, {offset:20});
-    // });
-
-    // $('#phone').mask('(999) 999-9999');
-    // $('#zip').mask('99999');
-
-    // $('#delegate-form form').submit(function(event) {
-    // 	event.preventDefault();
-
-    // 	var $form = $( this ),
-    // 	rawPhoneNumber = $('#phone').val(),
-    // 	url = $form.attr( 'action' );
-
-    // 	rawPhoneNumber = rawPhoneNumber.replace(' ', '');
-    // 	rawPhoneNumber = rawPhoneNumber.replace('(', '');
-    // 	rawPhoneNumber = rawPhoneNumber.replace(')', '');
-    // 	rawPhoneNumber = rawPhoneNumber.replace('-', '');
-
-    //    // Phone number validation
-    //    if(rawPhoneNumber === null || rawPhoneNumber === '' || rawPhoneNumber.length !== 10) {
-    //    	$('.aqua-box .reduced-width p:nth-child(2)').css('display', 'block');
-    //    } else {
-    //      // var posting = $.post( url, { first_name: $('#first_name').val(), last_name: $('#last_name').val(), phone_number: $('#phone').val(), zip_code: $('#zip').val() } );
-    //      var requestData = { first_name: $('#first_name').val(), last_name: $('#last_name').val(), phone_number: $('#phone').val(), zip_code: $('#zip').val() } ;
-    //      var posting =
-    //      $.ajax(url, {
-    //      	data : JSON.stringify(requestData),
-    //      	contentType : 'application/json',
-    //      	type : 'POST',
-    //      	dataType: 'json'
-    //      });
-    //    }
-
-    //    posting.done(function( data ) {
-    //    	$('.aqua-box form').css('display', 'none');
-    //    	$('.aqua-box .reduced-width p:nth-child(2)').css('display', 'none');
-    //    	$('.aqua-box .reduced-width p:last-child').css('display', 'block');
-    //    	$('.aqua-box .reduced-width').css('margin-bottom', '60px');
-    //    });
-    //  });
+    $('.dismiss').click(function(e){
+      e.preventDefault();
+      $($(this).attr('href')).removeClass('animated');
+      $($(this).attr('href')).css('top', -howItWorksHeight);
+      $('.main').removeAttr('style');
+    });
 
   });
 })(jQuery);
