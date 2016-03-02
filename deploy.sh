5ebc750e3ed1f234dbaa4a9e3cca673ab5de983f
Austin Middleton <xuset1@gmail.com>
2016-03-01 21:36:40 -0600

    Invalidating cloudfront cache on deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+s3bucket="godelegateit.com"
+
+gulp clean
+gulp build
+
+echo "Pushing changes to $s3bucket"
+aws s3 sync www s3://$s3bucket --include "*" --delete --no-follow-symlinks --acl public-read
+
+echo "Invalidating cache in cloudfront"
+aws cloudfront create-invalidation --distribution-id E2QNP3K5NUAYBE --paths \*
+
