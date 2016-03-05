813dee4462918da1a9638ab16d678c0e4825dd64
Austin Middleton <xuset1@gmail.com>
2016-03-05 17:18:05 -0600

    Fixed deployment script

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ echo "Pushing changes to $s3bucket"
 aws s3 sync www s3://$s3bucket --include "*" --delete --no-follow-symlinks --acl public-read
 
 echo "Invalidating cache in cloudfront"
-aws cloudfront create-invalidation --distribution-id E2QNP3K5NUAYBE --paths \*
+aws cloudfront create-invalidation --distribution-id E2QNP3K5NUAYBE --paths "/*"
 
