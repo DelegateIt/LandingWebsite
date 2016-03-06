739ccc0403703f308df001c3d094e83dafe573f8
nathanchapman <nathanchapman@utexas.edu>
2016-03-05 21:19:37 -0600

    Update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,7 @@ gulp clean
 gulp build
 
 echo "Pushing changes to $s3bucket"
-aws s3 sync www s3://$s3bucket --include "*" --delete --no-follow-symlinks --acl public-read
+aws s3 sync dist s3://$s3bucket --include "*" --delete --no-follow-symlinks --acl public-read
 
 echo "Invalidating cache in cloudfront"
 aws cloudfront create-invalidation --distribution-id E2QNP3K5NUAYBE --paths "/*"
-
